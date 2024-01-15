d44ee48c404aeb722c9b06ee004899d89223660d
paulanirban <anirbanpaul2006@gmail.com>
2024-01-15 15:53:23 +0530

    added a couple of more problems for builder pattern

---
+++ src/com/practice/designpatterns/creational/builder/databaseconfiguration/WithBuilder.java
@@ -0,0 +1,11 @@
+package com.practice.designpatterns.creational.builder.databaseconfiguration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface WithBuilder {
+}
--- src/com/practice/designpatterns/creational/builder/messagingservice/MessageBuilder.java
+++
@@ -1,5 +0,0 @@
-package com.practice.designpatterns.creational.builder.messagingservice;
-
-@WithBuilder
-public class MessageBuilder {
-}
